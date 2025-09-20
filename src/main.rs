65260f01b3cddf76a7e9288c6594d7484d24f9ec
Dessalines <tyhou13@gmx.com>
2025-09-20 18:38:52 -0400

    Changing names.

--- src/main.rs
+++ src/main.rs
@@ -30,27 +30,37 @@ fn app() -> impl IntoView {
 /// An example of a show if component. Don't pass signals for static components, use move in the level above.
 /// This makes testing and debugging much easier.
 fn submitted_comment_form(comment_form: Option<CommentForm>) -> impl IntoView {
-    comment_form.map(|cf| div().child(format!("Comment: {}, Likes: {}", cf.comment, cf.likes)))
+    comment_form.map(|cf| {
+        div().child(format!(
+            "Submitted Comment: {}, Likes: {}",
+            cf.comment, cf.likes
+        ))
+    })
 }
 
 /// A sample comment form, which pushes up an on_save event with the form data.
-fn comment_form(on_save: impl Fn(CommentForm) + 'static) -> impl IntoView {
+fn comment_form(on_submit: impl Fn(CommentForm) + 'static) -> impl IntoView {
     let comment = RwSignal::new("".to_string());
 
     // The browser seems to demand strings, no numbers.
     let likes = RwSignal::new("".to_string());
 
-    let on_submit = move |ev: SubmitEvent| {
+    let form_submit = move |ev: SubmitEvent| {
         ev.prevent_default();
-        on_save(CommentForm {
+        on_submit(CommentForm {
             comment: comment.get(),
             likes: likes.get().parse().expect("is number"),
         })
     };
 
-    div().child(form().on(ev::submit, on_submit).child((
-        input().bind(Value, comment),
-        input().attr("type", "number").bind(Value, likes),
-        button().attr("type", "submit").child("Save"),
-    )))
+    div().child(
+        form().on(ev::submit, form_submit).child((
+            input().bind(Value, comment).placeholder("a comment"),
+            input()
+                .attr("type", "number")
+                .bind(Value, likes)
+                .placeholder("number of likes"),
+            button().attr("type", "submit").child("Save"),
+        )),
+    )
 }
